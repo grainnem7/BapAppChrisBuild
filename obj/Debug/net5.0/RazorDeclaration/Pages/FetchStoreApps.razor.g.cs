4b315c467656f6b7e62a534655949d3c327fc32e
GrainneMeyer <grain@DESKTOP-LGK6TQE>
2021-07-02 12:43:01 +0100

    fixed column names

--- obj/Debug/net5.0/RazorDeclaration/Pages/FetchStoreApps.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/FetchStoreApps.razor.g.cs
@@ -112,7 +112,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 58 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\FetchStoreApps.razor"
+#line 59 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\FetchStoreApps.razor"
        
 
     string responseBody = "";
