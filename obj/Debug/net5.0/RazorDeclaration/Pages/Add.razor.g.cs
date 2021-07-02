63056b04663ed7578e1c4153d8545b8a45bf24fd
GrainneMeyer <grain@DESKTOP-LGK6TQE>
2021-07-02 12:16:58 +0100

    Adding column names to table

--- obj/Debug/net5.0/RazorDeclaration/Pages/Add.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Add.razor.g.cs
@@ -13,105 +13,105 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 3 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 4 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 5 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 8 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 10 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 11 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Add.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 3 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Add.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Add.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Add.razor"
 using Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\kelvi\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Add.razor"
 using Microsoft.Extensions.Logging;
 
 #line default
