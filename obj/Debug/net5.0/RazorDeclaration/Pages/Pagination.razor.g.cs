7f977fd365023919103c802342ed7aed5608828f
GrainneMeyer <grain@DESKTOP-LGK6TQE>
2021-07-03 21:38:36 +0100

    layout changed

--- obj/Debug/net5.0/RazorDeclaration/Pages/Pagination.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Pagination.razor.g.cs
@@ -13,91 +13,91 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 3 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 4 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 5 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 8 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 10 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\_Imports.razor"
+#line 11 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
 using Data;
 
 #line default
@@ -112,7 +112,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 57 "C:\Users\ruper\source\repos\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 57 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
        
     string responseBody = "";
 
