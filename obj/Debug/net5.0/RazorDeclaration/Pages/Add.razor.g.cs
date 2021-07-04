d27f08ccd02bfcbd844a2506fa5f9ca71fb9ab78
Shannon McGuckin <smg2813@gmail.com>
2021-07-04 19:02:44 +0100

    added a div class the <editform>

--- obj/Debug/net5.0/RazorDeclaration/Pages/Add.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Add.razor.g.cs
@@ -13,105 +13,105 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 2 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 3 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 4 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 5 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 6 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 7 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 8 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 9 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 10 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 11 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 2 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 3 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 6 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 7 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
 using Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 9 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
 using Microsoft.Extensions.Logging;
 
 #line default
@@ -126,41 +126,39 @@ using Microsoft.Extensions.Logging;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 37 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+#line 43 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
  
-        private StoreApp storeApp = new();
+    private StoreApp storeApp = new();
 
-        private void HandleValidSubmit()
-        {
-            Logger.LogInformation("HandleValidSubmit Called");
-        }
+    private void SubmitApp()
+    {
+        Logger.LogInformation("SubmitApp called");
 
-    
+    }
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 109 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
-           
+#line 102 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Add.razor"
+       
 
-        string responseBody = "";
-        List<StoreApp> StoreApps = new List<StoreApp>();
+    string responseBody = "";
+    List<StoreApp> StoreApps = new List<StoreApp>();
 
-        protected override async Task OnInitializedAsync()
+    protected override async Task OnInitializedAsync()
+    {
+        var apiName = "api/StoreApps/CreateApp";
+        var httpResponse = await client.GetAsync(apiName);
+
+        if (httpResponse.IsSuccessStatusCode)
         {
-            var apiName = "api/StoreApps/CreateApp";
-            var httpResponse = await client.GetAsync(apiName);
-
-            if (httpResponse.IsSuccessStatusCode)
-            {
-                responseBody = await httpResponse.Content.ReadAsStringAsync();
-                StoreApps = JsonConvert.DeserializeObject<List<StoreApp>>(responseBody);
-                StateHasChanged();
-            }
-
+            responseBody = await httpResponse.Content.ReadAsStringAsync();
+            StoreApps = JsonConvert.DeserializeObject<List<StoreApp>>(responseBody);
+            StateHasChanged();
         }
-    
+
+    }
 
 #line default
 #line hidden
