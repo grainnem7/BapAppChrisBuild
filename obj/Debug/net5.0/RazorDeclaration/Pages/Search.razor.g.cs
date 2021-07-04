4738c0a3f871e0204588651a5087b18b3cb4816d
Caitlin O'Hara <crohara1@gmail.com>
2021-07-04 17:12:15 +0100

    Search functionality now working

--- obj/Debug/net5.0/RazorDeclaration/Pages/Search.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Search.razor.g.cs
@@ -13,98 +13,98 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 2 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 3 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 4 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 5 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 6 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 7 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 8 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 9 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 10 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 11 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
+#line 3 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
+#line 7 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
+#line 8 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
 using Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
+#line 10 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
 using Microsoft.Extensions.Logging;
 
 #line default
@@ -118,6 +118,46 @@ using Microsoft.Extensions.Logging;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 72 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Search.razor"
+       
+    private SearchModel searchModel = new();
+
+    //Logging Entries to Searchbox
+    private void SearchStore()
+    {
+        Logger.LogInformation("SearchStore called");
+        Logger.LogInformation(searchModel.Name);
+    }
+    public void EnterSearch()
+    {
+        GetSearch(searchModel.Name);
+    }
+
+    string responseBody = "";
+    List<StoreApp> StoreApps = new List<StoreApp>();
+
+    //get searchterm
+    public async Task GetSearch(string search)
+    {
+        var apiName = "api/StoreApps/Search?searchTerm=" + search;
+
+        var httpResponse = await client.GetAsync(apiName);
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            responseBody = await httpResponse.Content.ReadAsStringAsync();
+
+            StoreApps = JsonConvert.DeserializeObject<List<StoreApp>>(responseBody);
+
+            StateHasChanged();
+        }
+
+    }
+
+#line default
+#line hidden
+#nullable disable
         [global::Microsoft.AspNetCore.Components.InjectAttribute] private ILogger<Search> Logger { get; set; }
         [global::Microsoft.AspNetCore.Components.InjectAttribute] private HttpClient client { get; set; }
     }
