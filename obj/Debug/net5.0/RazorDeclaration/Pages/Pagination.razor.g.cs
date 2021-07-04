77f870408f917e7c16d574f5e6e5078c5afce6c6
GrainneMeyer <grain@DESKTOP-LGK6TQE>
2021-07-04 17:53:54 +0100

    removed search from navbar

--- obj/Debug/net5.0/RazorDeclaration/Pages/Pagination.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Pagination.razor.g.cs
@@ -13,92 +13,99 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 3 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 4 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 5 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 8 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 10 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Student\Desktop\bapteam01\BapBlazor\_Imports.razor"
+#line 11 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
 using Data;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
+using Microsoft.Extensions.Logging;
 
 #line default
 #line hidden
@@ -112,24 +119,38 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 65 "C:\Users\Student\Desktop\bapteam01\BapBlazor\Pages\Pagination.razor"
+#line 76 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Pagination.razor"
        
 
 
-        //public void SortDate()
-        //{
-        //    foreach (var storeapp in StoreApps)
+    private SearchModel searchModel = new();
 
-        //    {
-        //        String sourceDate = storeapp.Date;
-        //        DateTime dateNew = DateTime.Parse(storeapp.Date);
-        //        string formatted = dateNew.ToString("yyyy-MM-dd");
-        //        storeapp.Date = formatted;
-        //    }
-        //    StoreApps.Sort((x, y) => x.Date.CompareTo(y.Date));
-        //}
+    private void SearchStore()
+    {
+        Logger.LogInformation("SearchStore called");
+        Logger.LogInformation(searchModel.Name);
+    }
 
+    public void EnterSearch()
+    {
+        GetSearch(searchModel.Name);
+    }
 
+    public async Task GetSearch(string search)
+    {
+        var apiName = "api/StoreApps/Search?searchTerm=" + search;
+
+        var httpResponse = await client.GetAsync(apiName);
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            responseBody = await httpResponse.Content.ReadAsStringAsync();
+
+            StoreApps = JsonConvert.DeserializeObject<List<StoreApp>>(responseBody);
+
+            StateHasChanged();
+        }
+    }
 
 
     private string responseBody = "";
@@ -219,6 +240,7 @@ using Data;
 #line default
 #line hidden
 #nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private ILogger<Search> Logger { get; set; }
         [global::Microsoft.AspNetCore.Components.InjectAttribute] private HttpClient client { get; set; }
     }
 }
