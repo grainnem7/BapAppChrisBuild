1e098ed17acbe315496e1d771e98cecc34d348a6
Shannon McGuckin <smg2813@gmail.com>
2021-07-06 18:29:17 +0100

    added CreateApp to the apiName in the delete.razor page.

--- obj/Debug/net5.0/RazorDeclaration/Pages/Delete.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Delete.razor.g.cs
@@ -13,98 +13,98 @@ namespace BapBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 2 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 3 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 4 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 5 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 6 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 7 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 8 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 9 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using BapBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 10 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using BapBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\_Imports.razor"
+#line 11 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Delete.razor"
+#line 2 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Delete.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Delete.razor"
+#line 6 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Delete.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Delete.razor"
+#line 7 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Delete.razor"
 using Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Delete.razor"
+#line 9 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Delete.razor"
 using Microsoft.Extensions.Logging;
 
 #line default
@@ -119,34 +119,35 @@ using Microsoft.Extensions.Logging;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 77 "C:\Users\grain\OneDrive\Desktop\BapTeam01\BapBlazor\Pages\Delete.razor"
+#line 78 "C:\Users\ruper\OneDrive\Desktop\bapteam01\BapBlazor\Pages\Delete.razor"
      
-    private StoreApp storeApp = new();
+        private StoreApp storeApp = new();
 
-    private void SubmitApp()
-    {
-        Logger.LogInformation("SubmitApp called");
-
-    }
-
-    string responseBody = "";
-    List<StoreApp>
-        StoreApps = new List<StoreApp>
-            ();
-
-    protected override async Task OnInitializedAsync()
-    {
-        var apiName = "api/StoreApps/";
-        var httpResponse = await client.GetAsync(apiName);
-
-        if (httpResponse.IsSuccessStatusCode)
+        private void SubmitApp()
         {
-            responseBody = await httpResponse.Content.ReadAsStringAsync();
-            StoreApps = JsonConvert.DeserializeObject<List<StoreApp>>(responseBody);
-            StateHasChanged();
+            Logger.LogInformation("SubmitApp called");
+
         }
 
-    }
+        string responseBody = "";
+        List<StoreApp>
+            StoreApps = new List<StoreApp>
+                ();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var apiName = "api/StoreApps/";
+            var httpResponse = await client.GetAsync(apiName);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                responseBody = await httpResponse.Content.ReadAsStringAsync();
+                StoreApps = JsonConvert.DeserializeObject<List<StoreApp>>(responseBody);
+                StateHasChanged();
+            }
+
+        }
+    
 
 #line default
 #line hidden
